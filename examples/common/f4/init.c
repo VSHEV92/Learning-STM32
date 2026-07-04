741fa5effbd1d5a9a83d77a04a2ba7df1cfde46e
VSHEV92 <59792635+VSHEV92@users.noreply.github.com>
2026-07-04 23:34:34 +0300

    Update init.c

--- examples/common/f4/init.c
+++ examples/common/f4/init.c
@@ -1,12 +1,32 @@
 #include "stm32f4xx.h"
-
+/*
+ * Frequency of HCLK in Hz. 
+ * Used to setup Systick. 
+ * Default Systick period is 1 ms.
+ */
 uint32_t SystemCoreClock = 16000000;
+
+/*
+ * This is encoding of HPRE feild of RCC_CFGR register.
+ * Array values encode AHB prescaler value as power of two.
+ * Prescaler input is HSE, HSI or PLL.
+ * Prescaler output is HCLK.
+ */ 
 const uint8_t AHBPrescTable[16] = {0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 6, 7, 8, 9};
+
+/*
+ * This is encoding of PPRE feild of RCC_CFGR register.
+ * Array values encode APB prescaler value as power of two.
+ * Prescaler input is HCLK.
+ * Prescaler output is APB CLK.
+ */ 
 const uint8_t APBPrescTable[8]  = {0, 0, 0, 0, 1, 2, 3, 4};
 
+/* 
+ * Most earlier system init callback.
+ * Called in startup.s right after stack setup.
+*/
 void SystemInit(void) {
-    // FPU settings
-    #if (__FPU_PRESENT) && (__FPU_USED)
-        SCB->CPACR |= ((3UL << 10*2)|(3UL << 11*2));  /* set CP10 and CP11 Full Access */
-    #endif
+    // FPU settings. Set CP10 and CP11 Full Access
+    SCB->CPACR |= ((3UL << 10*2)|(3UL << 11*2));
 }
