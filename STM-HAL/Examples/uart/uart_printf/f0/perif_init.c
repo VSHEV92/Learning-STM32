e85417aef5b7a42389cd83b3070555ba73746540
VSHEV92 <59792635+VSHEV92@users.noreply.github.com>
2026-08-15 23:58:30 +0300

    add printf example

---
+++ STM-HAL/Examples/uart/uart_printf/f0/perif_init.c
@@ -0,0 +1,36 @@
+#include "uart_printf.h"
+
+// UART2 Handler
+UART_HandleTypeDef huart2;
+
+void Peripheral_Init() {
+    
+    // Enable GPIO Port A and UART 2 clocks
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_USART2_CLK_ENABLE();
+
+
+    // Setup GPIO pins
+    GPIO_InitTypeDef GPIO_Init = {
+        .Pin       = GPIO_PIN_2|GPIO_PIN_15,     // choose GPIO pins
+        .Mode      = GPIO_MODE_AF_PP,            // set pins mode to alternative function
+        .Pull      = GPIO_NOPULL,                // disable pull up/down registers
+        .Speed     = GPIO_SPEED_FREQ_HIGH,       // set slew rate 
+        .Alternate = GPIO_AF1_USART2,            // choose UART2 as alternative function
+    };
+    HAL_GPIO_Init(GPIOA, &GPIO_Init);
+
+
+    // Initalize UART
+    huart2.Instance          = USART2;                 // uart interface instance
+    huart2.Init.BaudRate     = 9600;                   // buad rate
+    huart2.Init.WordLength   = UART_WORDLENGTH_8B;     // bits per symbol
+    huart2.Init.StopBits     = UART_STOPBITS_1;        // 1 stop bit
+    huart2.Init.Parity       = UART_PARITY_NONE;       // don't use parity bit
+    huart2.Init.Mode         = UART_MODE_TX;           // enable uart tx 
+    huart2.Init.HwFlowCtl    = UART_HWCONTROL_NONE;    // don't use hardware flow control
+    huart2.Init.OverSampling = UART_OVERSAMPLING_16;   // set oversampling to 16 bits  
+    HAL_UART_Init(&huart2);
+
+}
+
