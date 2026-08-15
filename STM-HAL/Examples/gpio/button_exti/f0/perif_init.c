3f0fbfb504e061340583761b8c61065da8a358bd
VSHEV92 <59792635+VSHEV92@users.noreply.github.com>
2026-08-15 23:23:59 +0300

    add all button examples

---
+++ STM-HAL/Examples/gpio/button_exti/f0/perif_init.c
@@ -0,0 +1,38 @@
+#include "button_exti.h"
+
+// external interrupt ISR
+void EXTI0_1_IRQHandler(void) {
+    HAL_GPIO_EXTI_IRQHandler(BUTTON_PIN);
+}
+
+void Peripheral_Init() {
+
+    // Enable GPIO Clocks
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+
+    // Initialize GPIO Led Pin
+    GPIO_InitTypeDef GPIO_InitStruct_Led = {
+        .Pin   = GPIO_PIN_3,             // pin number
+        .Mode  = GPIO_MODE_OUTPUT_PP,    // output push-pull mode
+        .Pull  = GPIO_NOPULL,            // disable pull up/down registers
+        .Speed = GPIO_SPEED_FREQ_LOW,    // set slew rate to low
+    };
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct_Led);
+    
+    // Initialize GPIO Button Pin
+    GPIO_InitTypeDef GPIO_InitStruct_Button = {
+        .Pin   = GPIO_PIN_0,             // pin number
+        .Mode  = GPIO_MODE_IT_RISING,    // external interrupt on rising edge mode
+        .Pull  = GPIO_PULLDOWN,          // enable pull up registers
+    };
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct_Button);
+
+    // Configure interrupts to maximum priority (lowest value)
+    HAL_NVIC_SetPriority(
+        EXTI0_1_IRQn, // IRQ number
+        0,            // Preempt Priority
+        0             // Sub Priority
+    );
+    HAL_NVIC_EnableIRQ(EXTI0_1_IRQn);
+}
