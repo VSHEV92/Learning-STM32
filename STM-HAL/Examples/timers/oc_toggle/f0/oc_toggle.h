0cad0303005f1bf7c5d264cb44b41e817d84c1ee
VSHEV92 <59792635+VSHEV92@users.noreply.github.com>
2026-08-16 09:20:49 +0300

    add other f0 timer examples

---
+++ STM-HAL/Examples/timers/oc_toggle/f0/oc_toggle.h
@@ -0,0 +1,8 @@
+#include "stm32f0xx_hal.h"
+
+void Peripheral_Init();
+
+extern TIM_HandleTypeDef htim2;
+
+#define EXEMPLE_TIMER htim2
+#define EXEMPLE_TIMER_OC_CHANNEL TIM_CHANNEL_2
---
+++ STM-HAL/Examples/timers/pwm_led/f0/perif_init.c
@@ -0,0 +1,46 @@
+#include "pwm_led.h"
+
+TIM_HandleTypeDef htim2;
+
+void Peripheral_Init() {
+
+    // Enable Clocks
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    __HAL_RCC_TIM2_CLK_ENABLE();
+
+    // Initialize GPIO pin
+    GPIO_InitTypeDef GPIO_InitStruct = {
+        .Pin   = GPIO_PIN_3,             // pin number
+        .Mode  = GPIO_MODE_AF_PP,        // alternstive function push-pull mode
+        .Pull  = GPIO_NOPULL,            // disable pull up/down registers
+        .Speed = GPIO_SPEED_FREQ_LOW,    // set slew rate to low
+        .Alternate = GPIO_AF2_TIM2,      // connect GPIO pin to Timer 2
+    };
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+    
+    
+    // Initialize Timer
+    htim2.Instance               = TIM2;                            // set timer instance  
+    htim2.Init.Prescaler         = 800 - 1;                         // set timer clock prescaler
+    htim2.Init.CounterMode       = TIM_COUNTERMODE_UP;              // set counter to UP mode 
+    htim2.Init.Period            = 100 - 1;                         // set timer period
+    htim2.Init.ClockDivision     = TIM_CLOCKDIVISION_DIV1;          // set timer clock division of sampling clock 
+    htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;  // set disable shadow preload register 
+    HAL_TIM_Base_Init(&htim2);
+
+    TIM_OC_InitTypeDef OC_Config = {
+        .OCMode     = TIM_OCMODE_PWM1,     // set output to PWM mode
+        .Pulse      = 0,                   // timer capture value
+        .OCPolarity = TIM_OCPOLARITY_HIGH, // don't invert OC output 
+    };
+    HAL_TIM_OC_ConfigChannel(&htim2, &OC_Config, TIM_CHANNEL_2);
+
+    
+    // Set timer clock source to internal clock
+    TIM_ClockConfigTypeDef sClockSourceConfig = {
+          .ClockSource = TIM_CLOCKSOURCE_INTERNAL,
+    };
+    HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig);
+
+}
+
