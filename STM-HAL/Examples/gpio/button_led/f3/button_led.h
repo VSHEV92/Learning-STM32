6031aee04469aad8ce81cc1f481ca2f9df8d3483
VSHEV92 <59792635+VSHEV92@users.noreply.github.com>
2026-08-23 18:08:18 +0300

    start discovery f3 examples

---
+++ STM-HAL/Examples/gpio/button_led/f3/button_led.h
@@ -0,0 +1,9 @@
+#include "stm32f3xx_hal.h"
+
+#define LED_PORT GPIOE
+#define LED_PIN GPIO_PIN_15
+
+#define BUTTON_PORT GPIOA
+#define BUTTON_PIN GPIO_PIN_0
+
+void Peripheral_Init();
---
+++ STM-HAL/Examples/gpio/button_led/f3/perif_init.c
@@ -0,0 +1,25 @@
+
+#include "button_led.h"
+
+void Peripheral_Init() {
+    // Enable GPIO Clocks
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOE_CLK_ENABLE();
+
+    // Initialize GPIO Led Pin
+    GPIO_InitTypeDef GPIO_InitStruct_Led = {
+        .Pin   = GPIO_PIN_15,             // pin number
+        .Mode  = GPIO_MODE_OUTPUT_PP,    // output push-pull mode
+        .Pull  = GPIO_NOPULL,            // disable pull up/down registers
+        .Speed = GPIO_SPEED_FREQ_LOW,    // set slew rate to low
+    };
+    HAL_GPIO_Init(GPIOE, &GPIO_InitStruct_Led);
+    
+    // Initialize GPIO Button Pin
+    GPIO_InitTypeDef GPIO_InitStruct_Button = {
+        .Pin   = GPIO_PIN_0,             // pin number
+        .Mode  = GPIO_MODE_INPUT,        // input mode
+        .Pull  = GPIO_NOPULL,            // disable pull up/down registers
+    };
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct_Button);
+}
